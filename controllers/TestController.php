f43f086606c1b9e81bc0217d4b724cb340ada74c
Ebu Enes YILDIRIM <eyildirim@xeonserver.com>
2025-05-20 18:04:18 +0300

    ok

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -2,23 +2,31 @@
 
 require 'app/function/Database.php'; //! Mysql
 
+
 class TestController
 {
     public function home($req = [])
     {
         $return = {
-            "title"
+            'title': 'Anasayfa Get - api - Mysql - Php',
+            'table': 'test',
+            'status': 1,
+            'msg': 'Anasayfa',
+            'size': 0,
+            'DB': [],
         }
+
+        
     }
 
     public function all($req = [])
     {
         
         $test = DB::table('test')
-            ->join('users', 'test.created_byId', '=', 'users.id')
-            ->select('test.id', 'test.title', 'users.name as user_name','users.departman')
+            //->join('users', 'test.created_byId', '=', 'users.id')
+            //->select('test.id', 'test.title', 'users.name as user_name','users.departman')
             //->where('test.deleted_status', '=', 0)
-            ->orderBy('id', 'ASC')
+            ->orderBy('test.id', 'ASC')
             //->limit($perPage)
             //->offset($offset)
             ->get(true); // true = JSON olarak döner
