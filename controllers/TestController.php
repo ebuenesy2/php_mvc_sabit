c7d5b1ddd741e0bcb9979569fd7c0dc6a4b33d99
EBU ENES YILDIRIM <86044516+ebuenesy2@users.noreply.github.com>
2025-05-21 01:17:19 +0300

    ok

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -5,24 +5,31 @@ require 'app/function/Database.php'; //! Mysql
 
 class TestController
 {
+
+    private static $tableName = "test"; //! Sql Tablo Name
+
     public function home($req = [])
     {
-        $return = {
-            'title': 'Anasayfa Get - api - Mysql - Php',
-            'table': 'test',
-            'status': 1,
-            'msg': 'Anasayfa',
-            'size': 0,
-            'DB': [],
-        }
-
+        //! Return
+        $return = array(
+            'title'=> 'Anasayfa Get - api - Mysql - Php',
+            'table'=> self::$tableName,
+            'status' => 1,
+            'msg' => 'Anasayfa',
+            'size' => 0,
+            'DB'=> [],
+        );
+        
+        echo json_encode($return);
         
     }
 
     public function all($req = [])
     {
-        
-        $test = DB::table('test')
+
+        print_r($req); die();
+
+        $test = DB::table(self::$tableName)
             //->join('users', 'test.created_byId', '=', 'users.id')
             //->select('test.id', 'test.title', 'users.name as user_name','users.departman')
             //->where('test.deleted_status', '=', 0)
