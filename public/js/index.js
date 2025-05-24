a21bc97f61cb3e950de44bfdf2477e55a6fb50e2
Ebu Enes YILDIRIM <eyildirim@xeonserver.com>
2025-05-24 15:35:33 +0300

    ok

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Buton referansını al
     const button = document.getElementById('myButton');
-    const message = document.getElementById('message');
-
+    
     // Butona tıklanınca mesajı değiştir
     button.addEventListener('click', function () {
-        message.textContent = "Butona tıkladınız! 🎉";
-        message.style.color = "green";
+        alert("tiklama");
     });
 });
